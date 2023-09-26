6901879ec4b96ac43b3555e92bb2cc2246be8a2d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-26 15:26:23 +0200

    dev: using typescript

--- front/src/main.ts
+++ front/src/main.ts
@@ -5,21 +5,27 @@ const multiplicationFactor = 2;
 
 // add the small circles (samples)
 const sampleContainer = document.querySelector("g.samples");
+if (sampleContainer === null) {
+  throw new Error("oups");
+}
 for (let i = 0; i < samples; i++) {
   const angle = (i * 2 * Math.PI) / samples - Math.PI / 2;
   const cx = cx0 + r0 * Math.cos(angle);
   const cy = cy0 + r0 * Math.sin(angle);
 
   const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", cx);
-  circle.setAttributeNS(null, "cy", cy);
-  circle.setAttributeNS(null, "r", 1);
+  circle.setAttributeNS(null, "cx", cx + "");
+  circle.setAttributeNS(null, "cy", cy.toString());
+  circle.setAttributeNS(null, "r", "1");
 
   sampleContainer.appendChild(circle);
 }
 
 // add the lines
 const lineContainer = document.querySelector("g.lines");
+if (lineContainer === null) {
+  throw new Error("oups");
+}
 for (let i = 0; i < samples; i++) {
   const angle1 = (i * 2 * Math.PI) / samples - Math.PI / 2;
   const x1 = cx0 + r0 * Math.cos(angle1);
@@ -30,10 +36,10 @@ for (let i = 0; i < samples; i++) {
   const y2 = cy0 + r0 * Math.sin(angle2);
 
   const line = document.createElementNS(svgns, "line");
-  line.setAttributeNS(null, "x1", x1);
-  line.setAttributeNS(null, "y1", y1);
-  line.setAttributeNS(null, "x2", x2);
-  line.setAttributeNS(null, "y2", y2);
+  line.setAttributeNS(null, "x1", x1 + "");
+  line.setAttributeNS(null, "y1", y1 + "");
+  line.setAttributeNS(null, "x2", x2 + "");
+  line.setAttributeNS(null, "y2", y2 + "");
 
   lineContainer.appendChild(line);
 }
