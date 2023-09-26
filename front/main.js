f31a0d6dfb14afe25bdc114a15508bb5267ad13b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-26 11:47:41 +0200

    dev: added maths for cx and cy

--- front/main.js
+++ front/main.js
@@ -1,12 +1,18 @@
 console.log("coucou");
 
+const cx0 = 0;
+const cy0 = 0;
+const r0 = 55;
+
 const svgns = "http://www.w3.org/2000/svg";
 const samples = 10;
 const container = document.querySelector("g.samples");
 
 for (let i = 0; i < samples; i++) {
-  const cx = 45;
-  const cy = -32 + 3 * i;
+  const angle = (i * 2 * Math.PI) / samples - Math.PI / 2;
+  const cx = cx0 + r0 * Math.cos(angle);
+  const cy = cy0 + r0 * Math.sin(angle);
+
   const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", cx);
   circle.setAttributeNS(null, "cy", cy);
