c13bcb3bc82489c357bb92a7a67add5b741f4e07
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-26 12:19:34 +0200

    refactor: added constant.js

---
+++ front/constant.js
@@ -0,0 +1,5 @@
+const cx0 = 0;
+const cy0 = 0;
+const r0 = 55;
+
+const svgns = "http://www.w3.org/2000/svg";
--- front/main.js
+++ front/main.js
@@ -1,15 +1,10 @@
 console.log("coucou");
 
-const cx0 = 0;
-const cy0 = 0;
-const r0 = 55;
-
-const svgns = "http://www.w3.org/2000/svg";
 const samples = 10;
 const multiplicationFactor = 2;
-const container = document.querySelector("g.samples");
 
 // add the small circles (samples)
+const sampleContainer = document.querySelector("g.samples");
 for (let i = 0; i < samples; i++) {
   const angle = (i * 2 * Math.PI) / samples - Math.PI / 2;
   const cx = cx0 + r0 * Math.cos(angle);
@@ -20,7 +15,7 @@ for (let i = 0; i < samples; i++) {
   circle.setAttributeNS(null, "cy", cy);
   circle.setAttributeNS(null, "r", 1);
 
-  container.appendChild(circle);
+  sampleContainer.appendChild(circle);
 }
 
 // add the lines
