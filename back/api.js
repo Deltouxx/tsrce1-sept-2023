b414dcb04c87957403500f82c01966be915dfdde
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-27 16:44:50 +0200

    dev: added nodemon
    commands:
    npm i nodemon -D

--- back/api.js
+++ back/api.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express.Router();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/random-config", (req, res) => {
   res.json({ sample: 123, multiplicationfactor: 97 });
 });
